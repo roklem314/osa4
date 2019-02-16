86e09de5f4141228460053479107e2b751b3bfc4
JL <lab.galom@outlook.com>
2019-02-16 15:42:23 +0200

    4.1 blogilista - 4.2 blogilista

---
+++ models/note.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+
+const url = process.env.MONGODB_URI
+
+console.log('commecting to', url)
+
+mongoose.connect(url, { useNewUrlParser: true })
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connection to MongoDB:', error.message)
+  })
+
+  const blogSchema = mongoose.Schema({
+    title: String,
+    author: String,
+    url: String,
+    likes: Number
+  })
+  const Blog = mongoose.model('Blog', blogSchema)
+
+  blogSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('Blog', blogSchema)
