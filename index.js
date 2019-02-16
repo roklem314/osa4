bf43429835c67114ded9af647ba656e490c2133a
JL <lab.galom@outlook.com>
2019-02-16 16:27:21 +0200

    4.2 blogilista

--- index.js
+++ index.js
@@ -1,35 +1,9 @@
+const app = require('./app') // varsinainen Express-sovellus
 const http = require('http')
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const mongoose = require('mongoose')
-const Blog = require('./models/note')
+const config = require('./utils/config')
 
+const server = http.createServer(app)
 
-app.use(cors())
-app.use(bodyParser.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = process.env.PORT
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+server.listen(config.PORT, () => {
+  console.log(`Server running on port ${config.PORT}`)
 })
--- models/blog.js
+++ models/blog.js
@@ -1,17 +1,5 @@
 const mongoose = require('mongoose')
 
-const url = process.env.MONGODB_URI
-
-console.log('commecting to', url)
-
-mongoose.connect(url, { useNewUrlParser: true })
-  .then(result => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connection to MongoDB:', error.message)
-  })
-
   const blogSchema = mongoose.Schema({
     title: String,
     author: String,
